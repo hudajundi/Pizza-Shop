ff5704e4970f43efa40765762e5f7fcfe2aae7dc
ruba95 <ruba.aljundi95@gmail.com>
2021-12-30 17:10:05 +0200

    js

--- app.js
+++ app.js
@@ -4,19 +4,22 @@ alert('Pizza on the way!');
 
 var body = document.getElementById('body');
     
+var card = document.getElementsByClassName('card');
+
 
 body.addEventListener('click', clickOnPizza);
 
 
 
 function clickOnPizza(event){
-    // var the = document.createElement("alert");
     var artc = event.target.id;
-    if(artc === "pizza1"){
+    for (var i= 0 ; i < 8 ; i++ ) {
+    if(artc === `pizza-${i}`){
     // img.src = "assests/images/cart-updated.png";
     alert ('you choose pizza1')
     }
-
+    }
 } 
-addEventListener();
+
+
 
