5b27b14bdaefbd572442db3437acc005612baf0a
ruba95 <ruba.aljundi95@gmail.com>
2021-12-31 22:04:02 +0200

    fixed code

--- app.js
+++ app.js
@@ -1,25 +1,66 @@
 'use strict';
 
-alert('Pizza on the way!');
+// alert('Pizza on the way!');
 
-var body = document.getElementById('body');
+
     
 var card = document.getElementsByClassName('card');
 
+var cart = document.getElementById('cart');
 
-body.addEventListener('click', clickOnPizza);
+var totalCart = document.getElementById('theTotalCart')
+
+var theClickedProducts = {}; /// we put the products into a group to count the amount 
+
+
+// body.addEventListener('click', clickOnPizza);
 
 
 
 function clickOnPizza(event){
-    var artc = event.target.id;
-    for (var i= 0 ; i < 8 ; i++ ) {
-    if(artc === `pizza-${i}`){
-    // img.src = "assests/images/cart-updated.png";
-    alert ('you choose pizza1')
+    var productData = event.getAttribute('data-product');
+    
+    var get_productData = document.getElementById(`pizza-${productData}`);
+
+    var total_productData = parseFloat(get_productData.textContent); ///to change string to num
+
+    ////// to know how many times the user click on the product : 
+
+    var total_products = parseFloat(theClickedProducts[productData]) ;  
+
+    if (total_products) {
+
+        total_productData = total_products + total_productData
     }
-    }
+
+
+    theClickedProducts[productData] = total_productData;
+
+    cartFunction();
 } 
 
+ 
+
+function cartFunction() {
+var sum = 0;
+var count=0; 
+   
+
+    for ( var i=1; i <= 6; i++){
+        var price = parseFloat(theClickedProducts[i]);
+        
+
+        if (price){
+            sum += price;
+            count++;
+        }
+    }
 
 
+    var cart = document.getElementById('cart');
+
+cart.innerHTML = sum.toFixed(2);
+var totalCart = document.getElementById('theTotalCart')
+totalCart.innerHTML = count;
+
+}
